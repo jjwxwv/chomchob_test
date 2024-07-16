2a5e4f9a89f7c2c5a33320bf2036fbe473a46c93
jj <jjwxwv@hotmail.com>
2024-07-17 04:41:36 +0700

    first commit

---
+++ database/entity.js
@@ -0,0 +1,158 @@
+import { DataTypes, Sequelize } from "sequelize";
+const sequelize = new Sequelize();
+
+const User = sequelize.define("user", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  //must hash password for better security
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  phoneNumber: { type: DataTypes.STRING },
+});
+
+User.hasMany(Order);
+
+const Item = sequelize.define("item", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  itemName: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  itemDetail: { type: DataTypes.TEXT },
+  itemPrice: { type: DataTypes.DECIMAL(10, 2) },
+  openSaleDate: { type: DataTypes.DATE },
+  endSaleDate: { type: DataTypes.DATE },
+});
+
+Item.hasOne(DiscountForItem);
+Item.hasMany(BundleDetails);
+Item.hasMany(OrderDetail);
+
+const DiscountForItem = sequelize.define("discount_item", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  discountPrice: { type: DataTypes.DECIMAL(10, 2) },
+  startDate: { type: DataTypes.DATE, allowNull: false },
+  endDate: { type: DataTypes.DATE, allowNull: false },
+});
+
+DiscountForItem.belongsTo(Item, {
+  foreignKey: { name: "item_id", allowNull: false },
+});
+
+const Bundle = sequelize.define("bundle", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  bundleName: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  bundleDetail: { type: DataTypes.TEXT },
+  bundlePrice: { type: DataTypes.DECIMAL(10, 2) },
+  openSaleDate: { type: DataTypes.DATE },
+  endSaleDate: { type: DataTypes.DATE },
+});
+
+Bundle.hasMany(BundleDetails);
+Bundle.hasMany(OrderDetail);
+
+const BundleDetails = sequelize.define("bundle_details", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+});
+
+BundleDetails.belongsTo(Item, {
+  foreignKey: { name: "item_id", allowNull: false },
+});
+BundleDetails.belongsTo(Bundle, {
+  foreignKey: { name: "bundle_id", allowNull: false },
+});
+
+const Order = sequelize.define("order", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  orderDate: {
+    type: DataTypes.DATE,
+  },
+});
+
+Order.belongsTo(User, {
+  foreignKey: { name: "user_id", allowNull: false },
+});
+Order.hasMany(OrderDetail);
+
+const OrderDetail = sequelize.define("order_detail", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  quantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+});
+
+OrderDetail.belongsTo(Item, {
+  foreignKey: { name: "item_id", allowNull: true },
+});
+OrderDetail.belongsTo(Bundle, {
+  foreignKey: { name: "bundle_id", allowNull: true },
+});
+OrderDetail.belongsTo(Order, {
+  foreignKey: { name: "order_id", allowNull: false },
+});
+OrderDetail.hasOne(ItemCode);
+
+const ItemCode = sequelize.define("item_code", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  code: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+ItemCode.belongsTo(OrderDetail, {
+  foreignKey: { name: "order_details_id", allowNull: false },
+});
